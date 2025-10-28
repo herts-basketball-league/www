6c2f362d8c332b4cdbb8876aede96c45d00abd9e
Richard Herbert <richard@infoweb.co.uk>
2025-10-28 12:15:26 +0000

    Moved file locations

---
+++ netlify/functions/scheduled-deploy.js
@@ -0,0 +1,20 @@
+// https://www.netlify.com/blog/how-to-schedule-deploys-with-netlify/
+import fetch from 'node-fetch';
+import { schedule } from '@netlify/functions';
+
+const BUILD_HOOK = 'https://api.netlify.com/build_hooks/68244410651fbd3cd691548f'
+
+// Schedules the handler function to run daily at 02:12
+const handler = schedule( '12 2 * * *', async () => {
+	await fetch( BUILD_HOOK, {
+		method: 'POST'
+	} ).then(response => {
+		console.log('Build hook response:', response);
+	} )
+
+	return {
+		statusCode: 200
+	}
+} )
+
+export { handler }
